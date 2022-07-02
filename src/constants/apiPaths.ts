b5f9c77bac71b07ba74f908c58ec70348003acb3
Fruneen <fruneen@gmail.com>
2022-07-03 00:53:01 +0300

    feat: integrate s3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://g468x58y7h.execute-api.eu-central-1.amazonaws.com',
   bff: 'https://5nqn6l4kv7.execute-api.eu-central-1.amazonaws.com',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
