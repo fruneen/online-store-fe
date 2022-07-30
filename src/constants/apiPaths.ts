9e9e882a4fd0aee6b92fa39f8de9f1c8001d7a2d
Fruneen <fruneen@gmail.com>
2022-07-30 23:25:45 +0300

    feat: add path to deployed cart service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://g468x58y7h.execute-api.eu-central-1.amazonaws.com',
   bff: 'https://v0kuaqbuck.execute-api.eu-central-1.amazonaws.com',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://fruneen-cart-api-development.eu-central-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
