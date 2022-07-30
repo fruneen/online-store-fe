c4315034f70a582d05d5c10965330aa0a76339db
Fruneen <fruneen@gmail.com>
2022-07-30 16:28:17 +0300

    feat: connect uploading csv file to s3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,8 +2,8 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://5nqn6l4kv7.execute-api.eu-central-1.amazonaws.com',
+  import: 'https://g468x58y7h.execute-api.eu-central-1.amazonaws.com',
+  bff: 'https://v0kuaqbuck.execute-api.eu-central-1.amazonaws.com',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
