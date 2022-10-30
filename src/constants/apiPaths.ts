df5deb763fef80a45e621427b906707a3170bd8c
Evgeny <fruneen@gmail.com>
2022-10-30 14:49:45 +0300

    Authorization (#8)
    * feat: implement authorization process

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://afwpponoc5.execute-api.eu-central-1.amazonaws.com',
+  import: 'https://1c5lmcopic.execute-api.eu-central-1.amazonaws.com',
   bff: ' https://v0kuaqbuck.execute-api.eu-central-1.amazonaws.com',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
