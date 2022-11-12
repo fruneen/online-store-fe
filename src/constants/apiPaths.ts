84acba5d1010a91a1ea253ac8d2d0ad12e19a01e
Evgeny <fruneen@gmail.com>
2022-11-12 16:28:32 +0300

    feat: add photo to each item

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://1c5lmcopic.execute-api.eu-central-1.amazonaws.com',
-  bff: ' https://v0kuaqbuck.execute-api.eu-central-1.amazonaws.com',
+  bff: 'https://bff-api.paralect.com',
   cart: 'https://cart-api.paralect.com/api',
 };
 
