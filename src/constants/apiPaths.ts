093f3a3121b3715e8d7ff23970a67f70e3d99f69
Evgeny <fruneen@gmail.com>
2022-11-12 13:22:04 +0300

    Containerization (#10)
    * feat: change api path to cart

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://1c5lmcopic.execute-api.eu-central-1.amazonaws.com',
   bff: ' https://v0kuaqbuck.execute-api.eu-central-1.amazonaws.com',
-  cart: 'https://9m3md5iu7l.execute-api.eu-central-1.amazonaws.com/api',
+  cart: 'https://cart-api.paralect.com/api',
 };
 
 export default API_PATHS;
